f224a21c9f894a0fa64f33387504cef95173548f
NullPointerException <does.not@exist.com>
2022-11-06 20:24:03 +0100

    Sound Draft #1

--- game.py
+++ game.py
@@ -1,6 +1,36 @@
 import json
+import vpk
+import os
+import pygame
 
 levelspath = "Levels/"
+infraPaths = ["/home/" + os.getlogin() + "/.local/share/Steam/steamapps/common/infra"]
+infraPath = None
+musicVpk = None
+
+for path in infraPaths:
+	if os.path.exists(path):
+		infraPath = path
+
+def getSong(song):
+	if os.path.exists("tmp.wav"):
+		os.remove("tmp.wav")
+	tmpFile = open("tmp.wav", "wb")
+	tmpFile.write(musicVpk.get_file("sound/music/cracks.wav").read())
+	tmpFile.close()
+	sound = pygame.mixer.Sound("tmp.wav")
+	os.remove("tmp.wav")
+	return sound
+
+pygame.mixer.init()
+if infraPath != None:
+	print("Found INFRA Path: " + infraPath)
+	musicVpk = vpk.open(infraPath + "/infra/pak01_dir.vpk")
+
+	#getSong("sound/music/cracks.wav").play()
+	#getSong("sound/music/furnace.wav").play()
+else:
+	print("Did not find INFRA Path")
 
 file = open(levelspath+"start.json","rb")
 start = json.loads(file.read().decode("utf-8-sig"))
@@ -35,5 +65,7 @@ while True:
         #Position change and commands
 	if command[0] in level[position]["commands"]:
 		position = level[position]["commands"][command[0]]
+		if level[position]["playsound"]:
+			getSong(level[position]["playsound"]).play()
 	else:
 		print("You didn't know what to think of that action.\n")
