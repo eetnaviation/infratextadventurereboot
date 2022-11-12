dffcc7670bfee4b5230778c3984256f13af6984f
eetnaviation <vindipesa@gmail.com>
2022-11-12 15:57:33 +0200

    Fixed linux.sh

--- linux.sh
+++ linux.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set +v
 clear
-echo .
-echo Welcome! Please choose an option to start playing.
-echo .
-echo 1 - Play without sounds
-echo 2 - Play with sounds !!! THIS OPTION REQUIERES INFRA TO BE INSTALLED ON HOST SYSTEM !!!
-echo.
+echo ""
+echo "Welcome! Please choose an option to start playing."
+echo ""
+echo "1 - Play without sounds"
+echo "2 - Play with sounds !!! THIS OPTION REQUIERES INFRA TO BE INSTALLED ON HOST SYSTEM !!!"
+echo ""
 read option
 if [ $option -eq 1 ]:
 	clear
